6c6252a5b4fcd62c7238399c056f59b5eea489fd
eaveltre21189 <e.a.veltre@gmail.com>
2023-05-01 22:21:12 -0400

    updated app.py file

--- Surfs_Up/app.py
+++ Surfs_Up/app.py
@@ -1,30 +1,95 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    # Find the most recent date in the data set.
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the date and precipitation scores
+    precip_data = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= year_ago).\
+        order_by(Measurement.date).all()
+    
+    session.close()
+  
+    precip_dict = {}
+    for date in precip_data:
+        precip_dict[date["date"]] = date["prcp"] 
+
+    return jsonify(precip_dict)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Create a query to return the list of stations
+    stations = session.query(Station.station)
+
+    session.close()
+
+    station_list = []
+    for station in stations:
+        station_list.append(station[0])
+    
+    return jsonify(station_list)
+
+
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
